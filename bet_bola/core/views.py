c3191944d796a484562b837a46b9620af6d2a7d2
mushzinho <pabllobeg@gmail.com>
2017-10-28 17:43:34 -0200

    Home page view added

--- bet_bola/core/views.py
+++ bet_bola/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Home(View):
+
+    def get(self, request, *args, **kargs):
+        return HttpResponse("OK")
+
+
+
