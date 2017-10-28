faea8715895e4f0d0d26d40ac366e5c6c4aeb5ad
mushzinho <pabllobeg@gmail.com>
2017-10-28 16:27:29 -0200

    Changing models.py of User and Core to acomplish new class diagram changes

--- bet_bola/user/models.py
+++ bet_bola/user/models.py
@@ -5,11 +5,13 @@ from django.contrib.auth.models import User
 class Generic_User(User):
 	mobile_phone = models.CharField(max_length=25)
 	birthday = models.DateTimeField(null=True)
+	
+	class Meta:
+		abstract=True
 
 class Seller(Generic_User):
 	cpf = models.CharField(max_length=10)
-	adress = models.CharField(max_length=75)
+	address = models.CharField(max_length=75)
 
 class Punter(Generic_User):
 	pass
-	#my_bet_tickets
