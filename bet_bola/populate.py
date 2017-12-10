a64602bfe85f0d1c18022c85a40d92adad5fddc7
MushZinho <pabllobeg@hotmail.com>
2017-12-10 14:50:14 -0200

    populate added

---
+++ bet_bola/populate.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import django
+
+def main():
+    sys.path.append('C:\\DEV\\bet_bola\\bet_bola')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    django.setup()
+    from utils import utils
+    print("POPULATING")
+    utils.populating_bd()
+    print("POPULATING")
+
+
+if __name__ == '__main__':
+    main()
