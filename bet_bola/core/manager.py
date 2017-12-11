4025d196311f95c2a026d9e67afd95640c31eabf
sywrahg <sywrahgabriella@hotmail.com>
2017-12-11 01:25:08 -0300

    filter by cotations

--- bet_bola/core/manager.py
+++ bet_bola/core/manager.py
@@ -1,13 +1,17 @@
 from django.db.models.query import QuerySet
 from django.db.models import Q
+from django.db.models import Count
 
 class GamesQuerySet(QuerySet):
 
 	def able_games(self):
-		return self.all().filter( Q(status_game="FT")| Q(status_game="NS")| Q(status_game="LIVE") | Q(status_game="HT") | Q(status_game="ET") 
+
+		return self.all().annotate(cotations_count=Count('cotations')).filter(cotations_count__gt=0).filter( Q(status_game="FT")| Q(status_game="NS")| Q(status_game="LIVE") | Q(status_game="HT") | Q(status_game="ET") 
 			| Q(status_game="PT") | Q(status_game="BREAK") | Q(status_game="DELAYED"))
+																					
 
 GamesManager = GamesQuerySet.as_manager
+
 
 class CotationsQuerySet(QuerySet):
 
