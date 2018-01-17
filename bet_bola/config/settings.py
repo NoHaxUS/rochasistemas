79a0282d28249318ee88d4ca1a9c9da60a5befd6
MushZinho <pabllobeg@hotmail.com>
2018-01-17 09:22:34 -0200

    settings

--- bet_bola/config/settings.py
+++ bet_bola/config/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = ['bet-bola.herokuapp.com','localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
