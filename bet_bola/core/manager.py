9a66a15d11acd64b8e5de428c533e010bd8eea86
fabiano <fabiomsrs@gmail.com>
2018-01-24 02:18:36 -0200

    improving the request processing

--- bet_bola/core/manager.py
+++ bet_bola/core/manager.py
@@ -7,15 +7,15 @@ class GamesQuerySet(QuerySet):
 
 	def able_games(self):
 		return self.annotate(cotations_count=Count('cotations')).filter(cotations_count__gt=0).filter(Q(status_game="NS")| Q(status_game="LIVE") | Q(status_game="HT") | Q(status_game="ET") 
-			| Q(status_game="PT") | Q(status_game="BREAK") | Q(status_game="DELAYED")).filter(start_game_date__date__gte=datetime.datetime.now().today().date())
+			| Q(status_game="PT") | Q(status_game="BREAK") | Q(status_game="DELAYED")| Q(status_game="POSTP")).filter(start_game_date__date__gte=datetime.datetime.now().today().date())
 
 	def today_able_games(self):
 		return self.annotate(cotations_count=Count('cotations')).filter(cotations_count__gt=0).filter(Q(status_game="NS")| Q(status_game="LIVE") | Q(status_game="HT") | Q(status_game="ET") 
-			| Q(status_game="PT") | Q(status_game="BREAK") | Q(status_game="DELAYED")).filter(start_game_date__date=datetime.datetime.now().today().date())
+			| Q(status_game="PT") | Q(status_game="BREAK") | Q(status_game="DELAYED")| Q(status_game="POSTP")).filter(start_game_date__date=datetime.datetime.now().today().date())
 
 	def tomorrow_able_games(self):
 		return self.annotate(cotations_count=Count('cotations')).filter(cotations_count__gt=0).filter(Q(status_game="NS")| Q(status_game="LIVE") | Q(status_game="HT") | Q(status_game="ET") 
-			| Q(status_game="PT") | Q(status_game="BREAK") | Q(status_game="DELAYED")).filter(start_game_date__date=datetime.date.today() + datetime.timedelta(days=1) )
+			| Q(status_game="PT") | Q(status_game="BREAK") | Q(status_game="DELAYED") | Q(status_game="POSTP")).filter(start_game_date__date=datetime.date.today() + datetime.timedelta(days=1) )
 
 GamesManager = GamesQuerySet.as_manager
 
