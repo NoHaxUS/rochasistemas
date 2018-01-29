132b3749b3ace80331bd92803f58cbc837898a1d
MushZinho <pabllobeg@hotmail.com>
2018-01-29 20:40:25 -0200

    OK

--- bet_bola/up.py
+++ bet_bola/up.py
@@ -1,11 +1,11 @@
 import os
 import sys
-#mport django
+import django
 
 def main():
     #sys.path.append('C:\\DEV\\bet_bola2\\bet_bola')
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-    #django.setup()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    django.setup()
     from utils import up
     print("Start")
     up.consuming_championship_api()
