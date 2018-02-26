8120088caa6151a7aba9c7548e5c605d69ab56ee
MushZinho <pabllobeg@hotmail.com>
2018-02-26 08:22:34 -0300

    creating filter to order cotations

--- bet_bola/core/templatetags/filters.py
+++ bet_bola/core/templatetags/filters.py
@@ -5,9 +5,14 @@ from django.conf import settings
 register = template.Library()
 
 @register.filter(name='standard_cotations_order_by')
-def standard_cotations_order_by(queryset, args):
-    args = [x.strip() for x in args.split(',')]
-    return queryset.filter(is_standard=True).order_by(*args)
+def standard_cotations_order_by(queryset):
+
+    standard_cotations = queryset.filter(is_standard=True)
+    if standard_cotations:
+        cote = [standard_cotations.get('1'), standard_cotations.get('X'), standard_cotations.get('2')]
+        return cote
+    else:
+        return standard_cotations
 
 @register.filter(name='order_by')
 def order_by(queryset, args):
