eb48068bcc6824a1396d66e7a923fbbaf9acb292
MushZinho <pabllobeg@hotmail.com>
2018-06-28 21:02:47 -0300

    Cotation normalization added by default

--- bet_bola/core/cotations_restrictions.py
+++ bet_bola/core/cotations_restrictions.py
@@ -5,91 +5,101 @@ def is_excluded_cotation(cotation_name, kind):
     if kind.pk == 38:
         excluded_cotations = [
             'Acima 0.5',
+            'Acima 1.5',
+            'Acima 2.5',
+            'Acima 3.5',
+            'Acima 4.5',
+            'Acima 5.5',
+            'Abaixo 0.5',
+            'Abaixo 1.5',
             'Abaixo 2.5',
             'Abaixo 3.5',
             'Abaixo 4.5',
             'Abaixo 5.5',
-            'Abaixo 3.0',
-            'Abaixo 4.0',
-            'Abaixo 5.0',
         ]
 
-        if cotation_name in excluded_cotations:
+        if not cotation_name in excluded_cotations:
             is_excluded = True
 
     elif kind.pk == 12:
         excluded_cotations = [
-            'Acima 0.5', 
+            'Acima 0.5',
+            'Acima 1.5',
+            'Acima 2.5',
+            'Acima 3.5',
+            'Acima 4.5',
+            'Acima 5.5',
+            'Abaixo 0.5',
+            'Abaixo 1.5',
+            'Abaixo 2.5',
+            'Abaixo 3.5',
             'Abaixo 4.5',
             'Abaixo 5.5',
-            'Abaixo 6.5',
-            'Abaixo 7.5',
-            'Abaixo 8.5',
-            'Abaixo 9.5',
-            'Abaixo 4.0',
-            'Abaixo 5.0',
-            'Abaixo 6.0',
-            'Abaixo 7.0',
-            'Abaixo 8.0',
-            'Abaixo 9.0',
-            'Abaixo 4.75',
-            'Abaixo 5.75',
-            'Abaixo 6.75',
-            'Abaixo 7.75',
-            'Abaixo 8.75',
-            'Abaixo 9.75',
         ]
         
-        if cotation_name in excluded_cotations:
+        if not cotation_name in excluded_cotations:
             is_excluded = True
 
     elif kind.pk == 976204:
         excluded_cotations = [
-            'Abaixo 2.0',
-            'Abaixo 3.0',
-            'Abaixo 4.0',
-            'Abaixo 5.0',
+            'Acima 0.5',
+            'Acima 1.5',
+            'Acima 2.5',
+            'Acima 3.5',
+            'Acima 4.5',
+            'Acima 5.5',
+            'Abaixo 0.5',
+            'Abaixo 1.5',
             'Abaixo 2.5',
             'Abaixo 3.5',
             'Abaixo 4.5',
             'Abaixo 5.5',
         ]
 
-        if cotation_name in excluded_cotations:
+        if not cotation_name in excluded_cotations:
             is_excluded = True
 
     elif kind.pk == 976198:
         excluded_cotations = [
-            'Abaixo 2.0',
-            'Abaixo 3.0',
-            'Abaixo 4.0',
-            'Abaixo 5.0',
+            'Acima 0.5',
+            'Acima 1.5',
+            'Acima 2.5',
+            'Acima 3.5',
+            'Acima 4.5',
+            'Acima 5.5',
+            'Abaixo 0.5',
+            'Abaixo 1.5',
             'Abaixo 2.5',
             'Abaixo 3.5',
             'Abaixo 4.5',
             'Abaixo 5.5',
         ]
         
-        if cotation_name in excluded_cotations:
+        if not cotation_name in excluded_cotations:
             is_excluded = True
 
     elif kind.pk == 47:
         excluded_cotations = [
             'Acima 0.5',
+            'Acima 1.5',
+            'Acima 2.5',
+            'Acima 3.5',
+            'Acima 4.5',
+            'Acima 5.5',
+            'Abaixo 0.5',
+            'Abaixo 1.5',
             'Abaixo 2.5',
             'Abaixo 3.5',
             'Abaixo 4.5',
             'Abaixo 5.5',
-            'Abaixo 3.0',
-            'Abaixo 4.0',
-            'Abaixo 5.0',
         ]
 
-        if cotation_name in excluded_cotations:
+        if not cotation_name in excluded_cotations:
             is_excluded = True
 
 
     #elif kind.pk == 63:
     #    is_excluded = True
 
+    
     return is_excluded
