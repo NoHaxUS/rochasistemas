6d6ca85da4bd635b11ae002841442ea3cbfa8620
Pablo Henrique <pabllobeg@hotmail.com>
2018-09-28 15:02:04 -0300

    Solved a small bug

--- bet_bola/core/models.py
+++ bet_bola/core/models.py
@@ -367,7 +367,7 @@ class Reward(models.Model):
 
 
     def __str__(self):
-        return str(self.value)
+        return str(self.real_value)
 
     class Meta:
         verbose_name = 'Recompensa'
