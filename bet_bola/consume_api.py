7dd78246f0a82e6398d655937ebafb1a44fceb23
Pablo Henrique <pabllobeg@hotmail.com>
2018-11-04 12:01:39 -0200

    Daemonize

--- bet_bola/consume_api.py
+++ bet_bola/consume_api.py
@@ -1,9 +1,21 @@
 import os
 import sys
 import django
+import logging
+from daemonize import Daemonize
+from updater.up import start_consuming_updates
+
+pid = "test.pid"
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
+fh = logging.FileHandler("test.log", "w")
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+keep_fds = [fh.stream.fileno()]
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
-from updater.up import start_consuming_updates
-start_consuming_updates()
+daemon = Daemonize(app="test_app", pid=pid, action=start_consuming_updates, keep_fds=keep_fds)
+daemon.start()
