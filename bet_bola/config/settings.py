11e639ae43f4bd452303631f619c022cc433bc22
Pablo Henrique <pabllobeg@hotmail.com>
2018-11-08 16:25:04 -0200

    DB

--- bet_bola/config/settings.py
+++ bet_bola/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't9xein@q$yf$w+ks2m&hr&53j1n@rtyg7o(b1(-)ffz7nce-kg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 DB_SWAP_LOCAL = False
 
 ALLOWED_HOSTS = ['*']
