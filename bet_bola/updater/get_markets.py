d10480effc32d748a88ef4a7fc046837d8203857
Pablo Henrique <pabllobeg@hotmail.com>
2019-03-29 16:25:50 -0300

    Bet Translations Almost done

--- bet_bola/updater/get_markets.py
+++ bet_bola/updater/get_markets.py
@@ -1,14 +1,16 @@
 from core.models import Cotation, Game, Market
 from .market_translations import MARKET_TRANSLATIONS
 
-def get_translated_cotation_with_header(cotation_name):
+def get_translated_cotation_with_header_goals(cotation_name):
     TRANSLATE_TABLE = {
         'Over': 'Acima',
         'Under':'Abaixo',
         'Home':'Casa',
         'Away':'Fora',
-        'X':'Empate',
+        'Score Draw': 'Empate',
+        'No Goal': 'Nenhum Gol'
     }
+
     cotation_translated = cotation_name.strip()
     for header in TRANSLATE_TABLE.keys():
         cotation_translated = cotation_translated.replace(header, TRANSLATE_TABLE.get(header, header))
@@ -17,6 +19,8 @@ def get_translated_cotation_with_header(cotation_name):
 
 def get_translated_cotation_with_header_name(cotation_name):
     TRANSLATE_TABLE = {
+        'Yes':'Sim',
+        'No':'Não',
         'Draw':'Empate',
         '2nd Half':'2° Tempo',
         '1st Half':'1° Tempo',
@@ -27,17 +31,33 @@ def get_translated_cotation_with_header_name(cotation_name):
     return cotation_translated
 
 
+def get_translated_cotation_with_header_name_special(cotation_name):
+    TRANSLATE_TABLE = {
+        'Home': 'Casa',
+        'Away': 'Fora',
+        'To Win to Nil':'Ganhar sem tomar Gol',
+        'To Win Either Half': 'Ganhar Qualquer Etapa',
+        'To Win Both Halves': 'Ganhar Ambas Etapas',
+        'To Score in Both Halves': 'Marcar em Ambas Etapas'
+    }
+
+    cotation_translated = cotation_name.strip()
+    for header in TRANSLATE_TABLE.keys():
+        cotation_translated = cotation_translated.replace(header, TRANSLATE_TABLE.get(header, header))
+    return cotation_translated
+
+
+
 def get_translated_cotation_with_opp(cotation_name):
     TRANSLATE_TABLE = {
-        '1':'Casa',
-        '2': 'Fora',
-        'X':'Empate',
+        'or': 'ou',
         '1X':'Casa/Empate',
         'X2':'Empate/Fora',
         '12':'Casa/Fora',
         'Over': 'Acima',
         'Under':'Abaixo',
         'Draw':'Empate',
+        'No Goal': 'Sem Gols',
         'Yes': 'Sim',
         'No': 'Não',
         'Goals':'Gols',
@@ -49,19 +69,33 @@ def get_translated_cotation_with_opp(cotation_name):
         '2nd Half': '2° Tempo',
         'Tie':'Igual',
         'Even': 'Par',
-        'Odd':'Ímpar'
+        'Odd':'Ímpar',
+        'Home':'Casa',
+        'Away':'Fora',
+        'X':'Empate'
     }
     cotation_translated = cotation_name
     for header in TRANSLATE_TABLE.keys():
         cotation_translated = cotation_translated.replace(header, TRANSLATE_TABLE.get(header, header))
     return cotation_translated
-    
-}
+  
 
-def cotation_with_header(cotations, market_name, game_id):
+def get_translated_cotation_with_opp_standard(cotation_name):
+    TRANSLATE_TABLE = {
+        '1':'Casa',
+        '2': 'Fora',
+        'X':'Empate',
+    }
+    cotation_translated = cotation_name
+    for header in TRANSLATE_TABLE.keys():
+        cotation_translated = cotation_translated.replace(header, TRANSLATE_TABLE.get(header, header))
+    return cotation_translated
+
+
+def cotation_with_header_goals(cotations, market_name, game_id):
     for cotation in cotations:
         Cotation.objects.update_or_create(
-            name=get_translated_cotation_with_header(cotation['header']) + ' ' + cotation['goals'],
+            name=get_translated_cotation_with_header_goals(cotation['header'] + ' ' + cotation['goals']),
             game=Game.objects.get(pk=game_id),
             market=Market.objects.get_or_create(
                 name=MARKET_TRANSLATIONS.get(market_name, market_name),
@@ -75,7 +109,7 @@ def cotation_with_header(cotations, market_name, game_id):
 def cotation_with_header_opp(cotations, market_name, game_id):
     for cotation in cotations:
         Cotation.objects.update_or_create(
-            name=get_translated_cotation_with_header_opp(cotation['header']) + ' ' + cotation['opp'],
+            name=get_translated_cotation_with_opp(cotation['header']) + ' ' + cotation['opp'],
             game=Game.objects.get(pk=game_id),
             market=Market.objects.get_or_create(
                 name=MARKET_TRANSLATIONS.get(market_name, market_name),
@@ -89,7 +123,7 @@ def cotation_with_header_opp(cotations, market_name, game_id):
 def cotation_with_header_name(cotations, market_name, game_id):
     for cotation in cotations:
         Cotation.objects.update_or_create(
-            name=get_translated_cotation_with_header_name(cotation['header']) + ' - ' + cotation['name'],
+            name=get_translated_cotation_with_header_name(cotation['header'] + ' - ' + cotation['name']),
             game=Game.objects.get(pk=game_id),
             market=Market.objects.get_or_create(
                 name=MARKET_TRANSLATIONS.get(market_name, market_name),
@@ -100,11 +134,43 @@ def cotation_with_header_name(cotations, market_name, game_id):
         )
 
 
+def cotation_with_header_name_special(cotations, market_name, game_id):
+    allowed = ('To Win to Nil','To Win Either Half','To Win Both Halves','To Score in Both Halves')
+    for cotation in cotations:
+        if cotation['name'] in allowed:
+            Cotation.objects.update_or_create(
+                name=get_translated_cotation_with_header_name_special(cotation['header'] + ' - ' + cotation['name']),
+                game=Game.objects.get(pk=game_id),
+                market=Market.objects.get_or_create(
+                    name=MARKET_TRANSLATIONS.get(market_name, market_name),
+                )[0],
+                defaults={
+                    'price': cotation['odds'],
+                }
+            )
+
+
+
 def cotation_without_header(cotations, market_name, game_id):
 
     for cotation in cotations:
         Cotation.objects.update_or_create(
-            name=get_translated_cotation_with_header_opp(cotation['opp']),
+            name=get_translated_cotation_with_opp(cotation['opp']),
+            game=Game.objects.get(pk=game_id),
+            market=Market.objects.get_or_create(
+                name=MARKET_TRANSLATIONS.get(market_name, market_name)
+            )[0],
+            defaults={
+                'price': cotation['odds'],
+            }
+        )
+
+
+def cotation_without_header_standard(cotations, market_name, game_id):
+
+    for cotation in cotations:
+        Cotation.objects.update_or_create(
+            name=get_translated_cotation_with_opp_standard(cotation['opp']),
             game=Game.objects.get(pk=game_id),
             market=Market.objects.get_or_create(
                 name=MARKET_TRANSLATIONS.get(market_name, market_name)
