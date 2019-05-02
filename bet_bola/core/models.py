96812181e8cd904c892c7e0d723af71c729fd7e4
Pablo Henrique <pabllobeg@hotmail.com>
2019-05-02 15:03:14 -0300

    Core models small changes

--- bet_bola/core/models.py
+++ bet_bola/core/models.py
@@ -15,13 +15,6 @@ from user.models import Seller
 from ticket.models import Ticket
 
 
-SETTLEMENT_STATUS = (
-            (0, "Em Aberto"),
-            (-1, "Cancelada"),
-            (1, "Perdeu"),
-            (2, "Ganhou")
-        )
-
 class Store(models.Model):
     fantasy = models.CharField(max_length=150, verbose_name="Nome da Banca")
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")    
@@ -87,10 +80,10 @@ class Game(models.Model):
     league = models.ForeignKey('League', related_name='my_games',null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Liga')
     sport = models.ForeignKey('Sport', related_name='my_games',null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Esporte')
     game_status = models.IntegerField(choices=GAME_STATUS,verbose_name='Status do Jogo')
-    results_calculated = models.BooleanField(default=False, verbose_name='Resultados Calculados?')
+    results_calculated = models.BooleanField(default=False, verbose_name='Resultados Calculados? ')
     visible = models.BooleanField(default=True, verbose_name='Visível?')
-    can_be_modified_by_api = models.BooleanField(default=True, verbose_name='API pode modificar?')
-    last_update = models.DateTimeField(verbose_name='Última Atualização', auto_now=True)
+    can_be_modified_by_api = models.BooleanField(default=True, verbose_name='API pode modificar? ')
+
 
     @property
     def standard_cotations(self):
@@ -105,28 +98,6 @@ class Game(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Period(models.Model):
-    
-    PERIOD_STATUS = (
-        (10,"Primeiro Tempo"),
-        (20,"Segundo Tempo"),
-        (30,"Primeiro Tempo (Prorrogacao)"),
-        (35,"Segundo Tempo (Prorrogacao)"),
-        (50,"Penaltis"),
-        (100,"Termino"),
-        (101,"Termino (Prorrogacao)"),
-        (102,"Termino (Penaltis")
-        )
-
-    id = models.BigAutoField(primary_key=True, verbose_name="ID")
-    period_type = models.IntegerField(choices=PERIOD_STATUS)
-    is_fineshed = models.BooleanField(default=False)
-    is_confirmed = models.BooleanField(default=False)
-    game = models.ForeignKey('Game',related_name='periods', on_delete=models.CASCADE)
-    home_score = models.IntegerField(default=0)
-    away_score = models.IntegerField(default=0)
     
 
 class League(models.Model):
@@ -165,7 +136,6 @@ class Location(models.Model):
 
 
 class Market(models.Model):
-    #id = models.BigIntegerField(primary_key=True, verbose_name="ID")
     name = models.CharField(max_length=100, verbose_name='Mercado', unique=True)
     available = models.BooleanField(default=True, verbose_name="Disponibilidade")
     
@@ -188,7 +158,13 @@ class CotationModified(models.Model):
 
 
 class Cotation(models.Model):
-       
+
+    SETTLEMENT_STATUS = (
+        (0, "Em Aberto"),
+        (1, "Perdeu"),
+        (2, "Ganhou"),
+        (3, 'Ao Vivo')
+    )
 
     id = models.BigAutoField(primary_key=True, verbose_name="ID")
     name = models.CharField(max_length=80, verbose_name='Nome da Cota')
@@ -198,27 +174,7 @@ class Cotation(models.Model):
     settlement = models.IntegerField(default=0, choices=SETTLEMENT_STATUS, verbose_name="Resultado")
     market = models.ForeignKey('Market', related_name='cotations', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Tipo da Cota')
     total_goals = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2, verbose_name='Total Acima/Abaixo')
-    last_update = models.DateTimeField(auto_now=True,null=True, blank=True, verbose_name="Última atualização")    
 
-    
-    def get_settlement_display_modified(self):
-        if self.game.game_status == 4:
-            return "Jogo Adiado"
-        elif self.game.game_status == 5:
-            return "Jogo Cancelado"            
-        elif self.game.game_status == 7:
-            return "Jogo Interrompido"
-        elif self.game.game_status == 8:
-            return "Jogo Abandonado"
-        elif self.game.game_status == 9:
-            return "Jogo retirado"
-        elif self.game.game_status == 99:
-            return "Jogo removido"
-        return self.get_settlement_display()
-
-    #def save(self, *args, **kwargs):
-    #   if not Cotation.objects.filter(id=self.id, status=3).exists():
- 
 
     def __str__(self):
         return str(self.id)
