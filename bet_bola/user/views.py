b6904971fe2a23581389980660162c80ed6a055d
Pablo Henrique <pabllobeg@hotmail.com>
2019-05-13 18:12:48 -0300

    Removing unused things

--- bet_bola/user/views.py
+++
@@ -1,9 +0,0 @@
-# from rest_framework.decorators import action
-# from rest_framework.viewsets import ModelViewSet, ViewSet
-# from rest_framework import permissions, mixins, generics, status
-# from rest_framework.response import Response
-# from user.models import Punter, NormalUser, CustomUser, Seller, Manager, Admin
-# from ticket.models import Ticket
-# from .serializers import PunterSerializer, NormalUserSerializer, SellerSerializer, ManagerSerializer, AdminSerializer
-# from .permissions import IsSuperUser, PunterViewPermission, SellerViewPermission, ManagerViewPermission, StoreGiven
-
