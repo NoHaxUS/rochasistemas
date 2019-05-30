710e8399aaa073c3fc8e99d602e891141c69b9af
Pablo Henrique <pabllobeg@hotmail.com>
2019-05-30 15:53:08 -0300

    Seller creation Working beatifully

--- bet_bola/user/my_views/seller.py
+++ bet_bola/user/my_views/seller.py
@@ -24,8 +24,6 @@ class SellerView(FiltersMixin, ModelViewSet):
 
     @action(methods=['post'], detail=True, permission_classes=[])
     def alter_credit(self, request, pk=None):
-        
-        print(request.data)
         credit =  int(request.data['credit'])
         seller = self.get_object()
         seller.alter_credit(credit)
@@ -44,26 +42,5 @@ class SellerView(FiltersMixin, ModelViewSet):
         return Response({'success': True})
 
 
-    @action(methods=['get'], detail=True)
-    def pay_seller(self, request, pk=None):
-        seller = self.get_object()
-        who_reseted_revenue = str(request.user.pk) + ' - ' + request.user.username
-        seller.reset_revenue(who_reseted_revenue)
 
-        return Response({'success':'Cambista Pago'})
-
-    @action(methods=['post'], detail=True)
-    def add_credit(self, request, pk=None):
-        try:
-            valor = request.data['value']
-        except KeyError:
-            return Response({"Error": "Entrada invalida. Dica:{'value':'?'}"})
-
-        instance = self.get_object()        
-        if request.user.has_perm('user.be_manager'):            
-            if request.user.manager.my_store.pk != instance.my_store.pk:
-                return Response({'failed':'Gerente não pertence a mesma loja que o vendedor em questão'})
-            instance.credit_limit += valor
-            credit_transation = request.user.manager.manage_credit(instance)
-            return Response(credit_transation)
 
--- bet_bola/user/serializers/seller.py
+++ bet_bola/user/serializers/seller.py
@@ -1,24 +1,32 @@
 from rest_framework import serializers
 from user.models import Manager, Seller, Punter
 from core.models import Store
+from django.utils.translation import gettext as _
 
 class SellerSerializer(serializers.HyperlinkedModelSerializer):	
 	
-	my_manager = serializers.SlugRelatedField(queryset=Manager.objects.all(), allow_null=True, slug_field='username')
+	my_manager = serializers.SlugRelatedField(queryset=Manager.objects.all(), allow_null=True, slug_field='username', error_messages={"does_not_exist": "{value} não existe."})
 	password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+
+	def create(self, validated_data):
+		#REVISE
+		#STORE IS STATIC
+		store = Store.objects.get(pk=1)
+		obj = Seller(**validated_data)
+		obj.my_store=store
+		obj.save()
+		return obj
+	
+
+	def validate_email(self, value):
+		if value:
+			if Seller.objects.filter(email=value):
+				raise serializers.ValidationError("Email ja cadastrado.")
+			return value
+		return value
+
 
 	class Meta:
 		model=Seller
 		fields = ('id','username','first_name','password','cellphone','address','cpf','can_sell_unlimited','credit_limit','limit_time_to_cancel','my_manager','email','can_cancel_ticket')
-
-	def create(self, validated_data):				
-		obj = Seller(**validated_data)
-		store = Store.objects.get(id=self.context['request'].GET.get('store'))
-		obj.my_store=store
-		obj.save()
-		return obj
-
-	def validate_email(self, value):
-		if Seller.objects.filter(email=value):
-			raise serializers.ValidationError("Email ja cadastrado.")
-		return value
