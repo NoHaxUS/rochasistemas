3b602bb0c2722664cc69bc010788ebad658824fa
fabiano <fabiomsrs@gmail.com>
2019-06-03 17:46:24 -0300

    revenue serializer

--- bet_bola/ticket/paginations.py
+++ bet_bola/ticket/paginations.py
@@ -14,3 +14,29 @@ class TicketPagination(PageNumberPagination):
             'total_pages': self.page.paginator.num_pages,            
             'results': data
         })
+
+
+class RevenuePagination(PageNumberPagination):
+    page_size = 30
+
+    def get_paginated_response(self, data):        
+        entry = 0
+        out = 0
+        sellers = []
+        for ticket in data:
+            entry += float(ticket["bet_value"])
+            if ticket["status"] == 'Venceu':
+                out += float(ticket["reward"]["value"])
+            sellers.append(ticket["payment"]["who_paid"])
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,            
+            'entry': entry,
+            'out': out,
+            'sellers': sellers,
+            'results': data
+        })
--- bet_bola/utils/my_views/revenue.py
+++ bet_bola/utils/my_views/revenue.py
@@ -4,8 +4,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from filters.mixins import FiltersMixin
 from ticket.models import Ticket, Reward, Payment
-from ticket.serializers.ticket import TicketSerializer, CreateTicketAnonymousUserSerializer, CreateTicketLoggedUserSerializer
-from ticket.paginations import TicketPagination
+from ticket.serializers.ticket import RevenueSerializer, TicketSerializer, CreateTicketAnonymousUserSerializer, CreateTicketLoggedUserSerializer
+from ticket.paginations import TicketPagination, RevenuePagination
 from ticket.permissions import CanCreateTicket, CanPayWinner, CanValidateTicket, CanCancelTicket, CanManipulateTicket
 from user.permissions import IsSuperUser
 from core.permissions import StoreIsRequired, UserIsFromThisStore
@@ -13,24 +13,25 @@ from user.models import TicketOwner
 from core.models import CotationCopy, Cotation, Store
 from utils.models import RewardRestriction
 from utils import timezone as tzlocal
+import datetime
 from config import settings
 
 class RevenueView(FiltersMixin, ModelViewSet):
     queryset = Ticket.objects.all()
-    serializer_class = TicketSerializer
+    serializer_class = RevenueSerializer
     permission_classes = (
         StoreIsRequired, 
         IsSuperUser|CanManipulateTicket, 
         IsSuperUser|CanCreateTicket
     )
-    pagination_class = TicketPagination
+    pagination_class = RevenuePagination
 
     filter_mappings = {
         'ticket_id':'pk',
-        'store':'store',
+        'store':'store__pk',
         'ticket_status':'status',
         'created_by': 'creator__username__icontains',
-        'paid_by': 'payment__who_paid__username__icontains',
+        'paid_by': 'payment__who_paid__username',
         'start_creation_date':'creation_date__gte',
         'end_creation_date':'creation_date__lte',
         'payment_status':'payment__status',
@@ -39,12 +40,10 @@ class RevenueView(FiltersMixin, ModelViewSet):
         'available': 'available',
     }
 
-    def list(self, request, pk=None):
-        from core.models import Store
-        store_id = request.GET.get('store')
-        store = Store.objects.get(pk=store_id)
-        tickets = Ticket.objects.filter(payment__status=2,store=store).exclude(status__in=[0,5,6])
+    def get_queryset(self):
+        date = datetime.date.today()
+        start_week = date - datetime.timedelta(date.weekday())
+        end_week = start_week + datetime.timedelta(7)
+        tickets = Ticket.objects.filter(payment__status=2, creation_date__range=[start_week, end_week]).exclude(status__in=[5,6])
+        return tickets
         
-        serializer = self.get_serializer(tickets, many=True)
-
-        return Response(serializer.data)
