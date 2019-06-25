14189a2611de4031b7bed4f3c88d78279ccf59ad
Pablo Henrique <pabllobeg@hotmail.com>
2019-06-25 18:25:19 -0300

    Ticket creation refactored

--- bet_bola/user/serializers/owner.py
+++ bet_bola/user/serializers/owner.py
@@ -2,25 +2,23 @@ from rest_framework import serializers
 from user.models import TicketOwner
 from core.models import Store
 
-class OwnerSerializer(serializers.HyperlinkedModelSerializer):			
-	
-	class Meta:
-		model=TicketOwner
-		fields = ('first_name','cellphone')
+class OwnerSerializer(serializers.HyperlinkedModelSerializer):
 
 	def create(self, validated_data):
 		request = self.context['request']
 		store = Store.objects.get(pk=request.GET.get('store'))
 		
-		if request.user.has_perm('be_punter'):
+		if request.user.has_perm('user.be_punter'):
 			data = {
 				'first_name': request.user.first_name,
 				'cellphone': request.user.cellphone
 			}
-			obj = TicketOwner(**data)
+			obj = TicketOwner.objects.create(my_store=store, **data)
 		else:
-			obj = TicketOwner(**validated_data)
+			obj = TicketOwner.objects.create(my_store=store, **validated_data)
 		
-		obj.my_store=store
-		obj.save()
 		return obj
+
+	class Meta:
+		model=TicketOwner
+		fields = ('first_name','cellphone')
