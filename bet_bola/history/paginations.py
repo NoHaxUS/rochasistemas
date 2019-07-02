1fb845e28e0f55b871c74a65629f205e851d3c8f
fabiano <fabiomsrs@gmail.com>
2019-07-02 12:01:55 -0300

    filter cancelation history

--- bet_bola/history/paginations.py
+++ bet_bola/history/paginations.py
@@ -2,11 +2,13 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from django.db.models import Count
 from user.models import CustomUser
+from ticket.models import Ticket
 
 class CancelationHistoryPagination(PageNumberPagination):
     page_size = 10
     paid_by_list = [{'id':user.pk,'username':user.username} for user in CustomUser.objects.filter(my_canceled_tickets_who_i_paid__isnull=False).distinct()]
     cancelled_by_list = [{'id':user.pk,'username':user.username} for user in CustomUser.objects.filter(my_canceled_tickets__isnull=False).distinct()]
+    ticket_id_list = [{'id':ticket.ticket_id} for ticket in Ticket.objects.filter(ticketcancelationhistory__isnull=False).distinct()]
 
     def get_paginated_response(self, data):        
         return Response({
@@ -18,6 +20,7 @@ class CancelationHistoryPagination(PageNumberPagination):
             'total_pages': self.page.paginator.num_pages,            
             'paid_by_list': self.paid_by_list,
             'cancelled_by_list': self.cancelled_by_list,
+            'ticket_id_list': self.ticket_id_list,
             'results': data
         })
 
@@ -25,6 +28,7 @@ class CancelationHistoryPagination(PageNumberPagination):
 class TicketValidationHistoryPagination(PageNumberPagination):
     page_size = 10
     paid_by_list = [{'id':user.pk,'username':user.username} for user in CustomUser.objects.filter(my_ticket_validations__isnull=False).distinct()]    
+
 
     def get_paginated_response(self, data):        
         return Response({
