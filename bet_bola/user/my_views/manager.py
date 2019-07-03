d1bc1bc52631d04fa332de04cc23ef43e94a9ba1
fabiano <fabiomsrs@gmail.com>
2019-07-03 16:04:22 -0300

    manager create method

--- bet_bola/user/my_views/manager.py
+++ bet_bola/user/my_views/manager.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
-from django.contrib import messages
+from rest_framework import status
 from rest_framework.response import Response
+from django.contrib import messages
 from user.models import Manager
 from core.permissions import StoreIsRequired, UserIsFromThisStore
 from user.permissions import IsAdmin
@@ -9,6 +10,7 @@ from user.serializers.manager import ManagerSerializer
 from core.paginations import StandardSetPagination
 from filters.mixins import FiltersMixin
 from rest_framework.permissions import IsAuthenticated
+import json
 
 class ManagerView(FiltersMixin, ModelViewSet):
     queryset = Manager.objects.filter(is_active=True)
@@ -21,6 +23,15 @@ class ManagerView(FiltersMixin, ModelViewSet):
         'login': 'username__icontains',
         'email': 'email__icontains'
     }
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.get('data')        
+        data = json.loads(data)        
+        serializer = self.get_serializer(data=data)               
+        serializer.is_valid(raise_exception=True)        
+        self.perform_create(serializer)                
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     @action(methods=['get'], detail=True, permission_classes=[])
     def toggle_is_active(self, request, pk=None):
