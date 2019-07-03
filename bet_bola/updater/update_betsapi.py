242537478d53d992a3459319db4a0ffa72edbc15
Pablo Henrique <pabllobeg@hotmail.com>
2019-07-03 19:19:56 -0300

    Changed to test

--- bet_bola/updater/update_betsapi.py
+++ bet_bola/updater/update_betsapi.py
@@ -21,10 +21,11 @@ def get_upcoming_events():
     url_base = "https://api.betsapi.com/v1/bet365/upcoming?sport_id=1&token=20445-s1B9Vv6E9VSLU1&day={0}&page={1}"
 
 
-    for index in range(0,4):
+    for index in range(-30,4):
         page = 1
         current_date = (base_day + datetime.timedelta(days=index)).strftime('%Y%m%d')
         current_url = url_base.format(current_date, page)
+        print(current_url)
         request = requests.get(current_url)
         data = request.json()
         if request.status_code == 200 and data['success'] == 1:
@@ -38,6 +39,7 @@ def get_upcoming_events():
 
         while page <= num_pages:
             current_url = url_base.format(current_date, page)
+            print(current_url)
             request = requests.get(current_url)
             if request.status_code == 200 and data['success'] == 1:
                 process_upcoming_events(request.json())
