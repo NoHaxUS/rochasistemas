e971309f4f7656d49092d2383abfbdf9a35f5335
Pablo Henrique <pabllobeg@hotmail.com>
2019-07-05 16:14:05 -0300

    Correcting the update script

--- bet_bola/up.py
+++ bet_bola/up.py
@@ -6,7 +6,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 django.setup()
 
 from updater.update_betsapi import get_upcoming_events
-from updater.process_results_betsapi import process_results
+from updater.process_results_betsapi import process_results, process_games
 
-get_upcoming_events()
+#get_upcoming_events()
 #process_results()
+#process_games
