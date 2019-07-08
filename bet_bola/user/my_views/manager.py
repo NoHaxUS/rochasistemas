265c342c9813d508f693625584d22d93a86a5f1e
fabiano <fabiomsrs@gmail.com>
2019-07-08 10:42:30 -0300

    permissions seller and manager

--- bet_bola/user/my_views/manager.py
+++ bet_bola/user/my_views/manager.py
@@ -17,7 +17,7 @@ class ManagerView(FiltersMixin, ModelViewSet):
     queryset = Manager.objects.filter(is_active=True)
     serializer_class = ManagerSerializer
     pagination_class = StandardSetPagination
-    permission_classes = []
+    permission_classes = [IsAdmin]
 
     filter_mappings = {
 		'store':'my_store',
@@ -36,7 +36,7 @@ class ManagerView(FiltersMixin, ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    @action(methods=['get'], detail=True, permission_classes=[])
+    @action(methods=['get'], detail=True, permission_classes=[IsAdmin])
     def toggle_is_active(self, request, pk=None):
         manager = self.get_object()        
         manager.toggle_is_active()
@@ -53,7 +53,7 @@ class ManagerView(FiltersMixin, ModelViewSet):
             'message':  'Alterado.'
         })
 
-    @action(methods=['post'], detail=True, permission_classes=[])
+    @action(methods=['post'], detail=True, permission_classes=[IsAdmin])
     def alter_credit(self, request, pk=None):
         data = json.loads(request.data.get('data'))
         credit = decimal.Decimal(data.get('credit'))
@@ -67,28 +67,28 @@ class ManagerView(FiltersMixin, ModelViewSet):
         
         return Response(response)                
 
-    @action(methods=['get'], detail=True, permission_classes=[])
+    @action(methods=['get'], detail=True, permission_classes=[IsAdmin])
     def toggle_can_cancel_ticket(self, request, pk=None):
         seller = self.get_object()
         seller.toggle_can_cancel_ticket()
         return Response({'success': True})
 
 
-    @action(methods=['get'], detail=True, permission_classes=[])
+    @action(methods=['get'], detail=True, permission_classes=[IsAdmin])
     def toggle_can_sell_unlimited(self, request, pk=None):
         seller = self.get_object()
         seller.toggle_can_sell_unlimited()
         return Response({'success': True})
 
 
-    @action(methods=['get'], detail=True, permission_classes=[])
+    @action(methods=['get'], detail=True, permission_classes=[IsAdmin])
     def toggle_can_change_limit_time(self, request, pk=None):
         seller = self.get_object()
         seller.toggle_can_change_limit_time()
         return Response({'success': True})
 
 
-    @action(methods=['get'], detail=True, permission_classes=[])
+    @action(methods=['get'], detail=True, permission_classes=[IsAdmin])
     def toggle_comission_based_on_profit(self, request, pk=None):
         seller = self.get_object()
         seller.toggle_comission_based_on_profit()
