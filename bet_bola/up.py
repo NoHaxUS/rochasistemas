6081f0bfc95a35c8ee5f323e5a826894a2dece70
Pablo Henrique <pabllobeg@hotmail.com>
2019-07-10 14:15:51 -0300

    Filters of games working beatifully

--- bet_bola/up.py
+++ bet_bola/up.py
@@ -9,7 +9,7 @@ from updater.update_betsapi import get_upcoming_events
 from updater.process_results_betsapi import process_results, process_games
 from updater.process_tickets import process_tickets
 
-#get_upcoming_events()
+get_upcoming_events()
 #process_results()
 #process_games()
-process_tickets()
+#process_tickets()
