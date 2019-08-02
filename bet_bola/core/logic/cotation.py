2680639afe66c8584265b669accaa84539ee2580
fabiano <fabiomsrs@gmail.com>
2019-08-02 14:24:07 -0300

    add cotation store price into ticket

---
+++ bet_bola/core/logic/cotation.py
@@ -0,0 +1,19 @@
+def get_store_price(self, store):
+    from utils.models import GeneralConfigurations
+    from core.models import CotationModified
+
+    config = store.my_configuration
+    
+    if self.price > config.max_cotation_value:
+        self.price = config.max_cotation_value					
+    if CotationModified.objects.filter(cotation=self, store=store, price__gt=1).exists():					
+        self.price = CotationModified.objects.filter(cotation=self, store=store).first().price
+    elif self.market.my_reduction.filter(store=store).exclude(reduction_percentual=100):							
+        self.price = self.price * self.market.my_reduction.get(store=store).reduction_percentual / 100										
+    else:					
+        self.price = self.price * config.cotations_percentage / 100													
+    
+    if self.price < 1:
+        self.price = 1.01
+
+    return self.price
