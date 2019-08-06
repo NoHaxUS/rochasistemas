ac1004df7a0ac4a893c79acff1d99fdd35357e0f
fabiano <fabiomsrs@gmail.com>
2019-08-06 17:48:20 -0300

    invalid cache view

--- bet_bola/core/views/games.py
+++ bet_bola/core/views/games.py
@@ -277,6 +277,13 @@ class TodayGamesAdmin(FiltersMixin, ModelViewSet):
         game_id = data.get('id')
         available = data.get('available')
         user = request.user
+        
+        invalidate_cache_group('today_games', request.user.my_store.pk)
+        invalidate_cache_group('tomorrow_games', request.user.my_store.pk)
+        invalidate_cache_group('after_tomorrow_games', request.user.my_store.pk)
+        invalidate_cache_group('search_games', request.user.my_store.pk)    
+        invalidate_cache_group('main_menu', request.user.my_store.pk) 
+
         if GameModified.objects.filter(game__pk=game_id, store=user.my_store).exists():
             GameModified.objects.filter(game__pk=game_id, store=user.my_store).update(available=available)
         else:
