660b65dc3c94f3e70436303cf49c0e650ea76b93
fabiano <fabiomsrs@gmail.com>
2019-08-14 16:26:40 -0300

    home

--- bet_bola/cashier/views/general_cashier.py
+++ bet_bola/cashier/views/general_cashier.py
@@ -23,28 +23,22 @@ class GeneralCashier(APIView):
         if request.user.is_authenticated:
             entries = 0
             out = 0
-            total_release = 0
+            won_bonus = 0
             comissions = 0
             total_out = 0
             user = request.user 
 
             if request.user.user_type == 2:
                 managers = Manager.objects.none()
-                sellers = Seller.objects.filter(pk=user.pk,payment__status=2, my_store=request.user.my_store).distinct()
-                for release in Entry.objects.filter(user=user).exclude(closed=True):
-                    total_release += release.value                
+                sellers = Seller.objects.filter(pk=user.pk,payment__status=2, my_store=request.user.my_store).distinct()                
 
             elif request.user.user_type == 3:
                 managers = Manager.objects.filter(pk=user.pk,manager_assoc__payment__status=2, my_store=request.user.my_store).distinct()
-                sellers = Seller.objects.filter(my_manager__pk=user.pk,payment__status=2, my_store=request.user.my_store).distinct()                
-                for release in Entry.objects.filter(Q(user=user) | Q(user__in=user.manager.manager_assoc.all())).exclude(closed=True):
-                    total_release += release.value
+                sellers = Seller.objects.filter(my_manager__pk=user.pk,payment__status=2, my_store=request.user.my_store).distinct()                                
 
             else:
                 managers = Manager.objects.filter(manager_assoc__payment__status=2, my_store=request.user.my_store).distinct()
-                sellers = Seller.objects.filter(payment__status=2, my_store=request.user.my_store).distinct()
-                for release in Entry.objects.filter(store=request.user.my_store):
-                    total_release += release.value     
+                sellers = Seller.objects.filter(payment__status=2, my_store=request.user.my_store).distinct()                
 
             for manager in ManagersCashierSerializer(managers, many=True, context={'request':self.request}).data:                        
                 comissions += manager['comission']            
@@ -52,7 +46,8 @@ class GeneralCashier(APIView):
 
             for seller in SellersCashierSerializer(sellers, many=True, context={'request':self.request}).data:            
                 entries += seller['entry']
-                out += seller['out'] + seller['won_bonus']
+                out += seller['out'] 
+                won_bonus += seller['won_bonus']
                 comissions += seller['comission']
                 total_out += seller['total_out']
 
@@ -60,9 +55,9 @@ class GeneralCashier(APIView):
                 
             data = {
                 'entries': entries,
-                'out': out,
-                'total_release': total_release,
+                'out': out,                
                 'comissions': comissions,
+                'won_bonus': won_bonus,
                 'total_out': total_out,
                 'profit': profit
             }    
