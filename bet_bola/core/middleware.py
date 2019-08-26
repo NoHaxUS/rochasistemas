6e585b1352731a9da6ab65c71dd906e6df97bcab
Pablo Henrique <pabllobeg@hotmail.com>
2019-08-26 13:44:52 -0300

    Middleware

--- bet_bola/core/middleware.py
+++ bet_bola/core/middleware.py
@@ -8,8 +8,11 @@ class ReadKeyMiddleware:
         self.get_response = get_response
     
     def __call__(self, request):
-        if not request.headers.get('SecurityAuthorization') == 'aW6dvtS4XvfYXutA':
-            if not settings.DEV_MODE:
+        
+        if 'admin' in request.path:
+            return self.get_response(request)
+
+        if not settings.DEV_MODE and not request.headers.get('SecurityAuthorization') == 'aW6dvtS4XvfYXutA':
                 return UnicodeJsonResponse({'status': 'failed'}, status=status.HTTP_403_FORBIDDEN)
-        response = self.get_response(request)
-        return response
+        
+        return self.get_response(request)
