61d3bdd18388214efd0ad045f774e7e4e3826b76
Pablo Henrique <pabllobeg@hotmail.com>
2019-08-28 09:01:53 -0300

    Added update script

---
+++ bet_bola/update_and_process.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+from updater.update_betsapi import get_upcoming_events
+from updater.process_results_betsapi import process_results, process_games
+from updater.process_tickets import process_tickets
+
+#get_upcoming_events()
+process_results()
+process_games()
+process_tickets()
