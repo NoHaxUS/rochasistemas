620720cf5c647f71565278aa0008f402880bfd3d
Pablo Henrique <pabllobeg@hotmail.com>
2019-09-02 15:48:38 -0300

    Home Pages

--- bet_bola/core/paginations.py
+++ bet_bola/core/paginations.py
@@ -24,8 +24,8 @@ class MyPaginator(Paginator):
             if count >= games_top:
                 top = league      
                 break
-            count+= self.object_list[league].games.__len__()
-                
+            count+= len(self.object_list[league].games)
+
         if top is None:                        
             return self._get_page(self.object_list[bottom:], number, self)
         return self._get_page(self.object_list[bottom:top], number, self)
@@ -35,7 +35,7 @@ class MyPaginator(Paginator):
         """Return the total number of objects, across all pages."""
         count = 0
         for league in self.object_list:            
-            count += league.games.__len__()        
+            count += len(league.games)      
         return count
 
     @cached_property
@@ -46,14 +46,14 @@ class MyPaginator(Paginator):
         
         hits = max(1, self.count - self.orphans)
         
-        if self.object_list and self.count - self.object_list[len(self.object_list) - 1].games.__len__() < self.per_page:
+        if self.object_list and self.count - len(self.object_list[len(self.object_list) - 1].games) < self.per_page:
             return ceil(hits / self.per_page)  - 1
         return ceil(hits / self.per_page)  
 
 
 class GameListPagination(PageNumberPagination):
     django_paginator_class = MyPaginator
-    page_size = 30
+    page_size = 40
 
     def get_paginated_response(self, data):                
 
