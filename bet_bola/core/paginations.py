aa310aa9a79ba3210830ec149825f1330d9a57e7
Pablo Henrique <pabllobeg@hotmail.com>
2019-09-02 14:50:45 -0300

    OK

--- bet_bola/core/paginations.py
+++ bet_bola/core/paginations.py
@@ -11,21 +11,20 @@ class MyPaginator(Paginator):
     def page(self, number):
         """Return a Page object for the given 1-based page number."""
         number = self.validate_number(number)
-        
         count = 0
         bottom = None
         top = None
         games_bottom = (number - 1) * self.per_page
         games_top = games_bottom + self.per_page
 
-        for l in range(len(self.object_list)):                        
+        for league in range(len(self.object_list)):                        
             if (games_bottom <= count and bottom is None) or (games_bottom == 0 and bottom is None):                
-                bottom = l
+                bottom = league
                 games_bottom = count
             if count >= games_top:
-                top = l         
+                top = league      
                 break
-            count+= self.object_list[l].games.__len__()
+            count+= self.object_list[league].games.__len__()
                 
         if top is None:                        
             return self._get_page(self.object_list[bottom:], number, self)
@@ -34,12 +33,9 @@ class MyPaginator(Paginator):
     @cached_property
     def count(self):
         """Return the total number of objects, across all pages."""
-        c = getattr(self.object_list, 'count', None)
-        if callable(c) and not inspect.isbuiltin(c) and method_has_no_args(c):
-            return c()
         count = 0
-        for l in self.object_list:            
-            count += l.games.__len__()        
+        for league in self.object_list:            
+            count += league.games.__len__()        
         return count
 
     @cached_property
@@ -47,12 +43,13 @@ class MyPaginator(Paginator):
         """Return the total number of pages."""
         if self.count == 0 and not self.allow_empty_first_page:
             return 0        
+        
         hits = max(1, self.count - self.orphans)
         
         if self.object_list and self.count - self.object_list[len(self.object_list) - 1].games.__len__() < self.per_page:
             return ceil(hits / self.per_page)  - 1
         return ceil(hits / self.per_page)  
-        
+
 
 class GameListPagination(PageNumberPagination):
     django_paginator_class = MyPaginator
