83eace1f9dc843d13cdb4e3c769cb3780d19e716
unknown <fabiomsrs@gmail.com>
2019-09-02 18:06:10 -0300

    fixing cotation price

--- bet_bola/core/logic/cotation.py
+++ bet_bola/core/logic/cotation.py
@@ -6,19 +6,19 @@ def get_store_price(self, store):
     config = store.my_configuration
     cotation_modified = CotationModified.objects.filter(cotation=self, store=store, price__gt=1)
     market_reducation = self.market.my_modifications.filter(store=store).exclude(reduction_percentual=100)    
-    price = 0
-    if self.price > config.max_cotation_value:
-        price = config.max_cotation_value
-    
+    price = self.price    
+
     if cotation_modified:
-        price = cotation_modified.first().price
+        price = cotation_modified.first().price    
+    elif market_reducation:						        	
+        price = price * market_reducation.first().reduction_percentual / 100										
+    else:        
+        price = price * config.cotations_percentage / 100													
     
-    elif market_reducation:							
-        price = self.price * market_reducation.first().reduction_percentual / 100										
-    else:
-        price = self.price * config.cotations_percentage / 100													
-    
-    if self.price < 1:
+    if price > config.max_cotation_value:
+        price = config.max_cotation_value
+        
+    if price < 1:
         price = 1.01
     
     return Decimal(str(price))
